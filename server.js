cbf322ecc0cef5376bad0619e7ec3c2142472b94
Andre-tech671 <nyanjahiaandre@gmail.com>
2026-08-22 22:10:29 +0200

    changing the url for api service to be to fetch the services

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const API_URL = "http://localhost:4000";
+const API_URL = process.env.API_URL || "http://localhost:4000";
+
 
 app.set('view engine', 'ejs');
 
